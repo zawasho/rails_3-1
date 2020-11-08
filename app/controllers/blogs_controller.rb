1b9633fe7b8241eca3548c33d16f268c82e7fdc6
zawasho <shoozawa20@gmail.com>
2020-11-08 03:12:43 +0000

    [Start] 3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,6 +27,12 @@ class BlogsController < ApplicationController
     blog.update(blog_params)
     redirect_to blog_path(blog)
   end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blog_path
+  end
 
   private
   def blog_params
